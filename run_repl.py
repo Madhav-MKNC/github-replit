f8eb7121c6c3497cb2db17fa9fa46d4ca71b3c61
Madhav Kumar <78339236+Madhav-MKNC@users.noreply.github.com>
2023-07-08 21:58:36 +0530

    Update run_repl.py

--- run_repl.py
+++ run_repl.py
@@ -1,24 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
 import requests
+import os
+import pyrepl
 
-# Set up the API key and repl ID
-api_key = "YOUR_API_KEY"
-repl_id = "YOUR_REPL_ID"
-
-# Define the base URL for the API
-base_url = "https://api.repl.it"
+REPLIT_API_TOKEN = os.environ['REPLIT_API_TOKEN']
+REPL_ID = "" # pyrepl.get_json()
+BASE_URL = "https://api.repl.it"
 
 # Run the main file in the repl
 def run_main_file(repl_id):
-    endpoint = f"{base_url}/v0/repls/{repl_id}/run"
-    headers = {"Authorization": f"Bearer {api_key}", "Content-Type": "application/json"}
+    endpoint = f"{BASE_URL}/v0/repls/{repl_id}/run"
+    headers = {"Authorization": f"Bearer {REPLIT_API_TOKEN}", "Content-Type": "application/json"}
     data = {"runMain": True}
     response = requests.post(endpoint, headers=headers, json=data)
     response_data = response.json()
     return response_data
 
-# Example: run the main file in the repl
-response_data = run_main_file(repl_id)
-print("Repl execution started!")
-print("Session ID:", response_data["id"])
-print("Console output:")
-print(response_data["consoleOutput"])
+if __name__ == "__main__":
+    response_data = run_main_file(repl_id)
+    print("Repl execution started!")
+    print("Session ID:", response_data["id"])
+    print("Console output:")
+    print(response_data["consoleOutput"])
